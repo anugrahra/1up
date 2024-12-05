0de7982df2961ffa0ce9e48d21951e36c9a17227
anugrahra <dialog.anugrah@gmail.com>
2024-12-05 10:23:52 +0700

    update bulanan

--- js/data_pac.js
+++ js/data_pac.js
@@ -1,2 +1,2 @@
-const labelPac = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agutus", "September", "Oktober"];
-const dataPac = [2950, 3900, 4100, 3450, 2950, 2700, 2475, 2350, 2375, 2475];
+const labelPac = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agutus", "September", "Oktober", "November"];
+const dataPac = [2950, 3900, 4100, 3450, 2950, 2700, 2475, 2350, 2375, 2475, 2725];
