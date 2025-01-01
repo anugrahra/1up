2e5bd0e8c0a03084c01ada00465692524a5f5090
anugrahra <dialog.anugrah@gmail.com>
2025-01-01 11:27:36 +0700

    update desember

--- js/data_kaporit.js
+++ js/data_kaporit.js
@@ -1,2 +1,2 @@
-const labelKaporit = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November"];
-const dataKaporit = [420, 405, 370, 305, 270, 270, 310, 380, 465, 520, 480];
+const labelKaporit = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+const dataKaporit = [420, 405, 370, 305, 270, 270, 310, 380, 465, 520, 480, 460];
--- js/data_produksi.js
+++ js/data_produksi.js
@@ -1,4 +1,4 @@
-const labelProduksi = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November"];
-const jumlahHari = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30];
-const dataProduksi = [139606, 141034, 144271, 126536, 135645, 120959, 131959, 134125, 127839, 137351, 138603];
-const waktuOperasi = [677, 654, 653, 614, 683, 636, 679, 699, 676, 703, 696];
+const labelProduksi = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+const jumlahHari = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const dataProduksi = [139606, 141034, 144271, 126536, 135645, 120959, 131959, 134125, 127839, 137351, 138603, 142645];
+const waktuOperasi = [677, 654, 653, 614, 683, 636, 679, 699, 676, 703, 696, 722];
